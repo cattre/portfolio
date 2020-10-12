60b690a6415baa0fbbf0ef4f256e3b74ab00f374
Richard Catterill <r_catterill@hotmail.com>
2020-10-12 23:12:12 +0100

    Updated intro and about page layout

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,15 @@
 let nameSpan = document.querySelector('.name')
 let name = ['R', 'i', 'c', 'h', 'a', 'r', 'd', ' ', 'C', 'a', 't', 't', 'e', 'r', 'i', 'l', 'l']
 let index = 0
+let fadeButtons = document.querySelectorAll('.fade')
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         typeName()
-    }, 1500)
+    }, 500)
+    fadeButtons.forEach(button => {
+        button.style.animation = 'fadein 1s forwards ease-in 2200ms'
+    })
 })
 
 typeName = () => {
@@ -17,8 +21,35 @@ typeName = () => {
         if(index >= 17) {
             clearInterval(typing)
         }
-    }, 200)
+    }, 100)
 }
+
+
+let intro = document.querySelector('.intro')
+let about = document.querySelector('.about')
+let portfolio = document.querySelector('.portfolio')
+
+let whoButton = document.querySelector('.who')
+let whatButton = document.querySelector('.what')
+let backButton = document.querySelector('.back')
+
+whoButton.addEventListener('click', () => {
+    intro.style.display = 'none'
+    about.style.display = 'flex'
+    portfolio.style.display = 'none'
+})
+
+whatButton.addEventListener('click', () => {
+    intro.style.display = 'none'
+    about.style.display = 'none'
+    portfolio.style.display = 'flex'
+})
+
+backButton.addEventListener('click', () => {
+    intro.style.display = 'flex'
+    about.style.display = 'none'
+    portfolio.style.display = 'none'
+})
 
 
 
--- portfolio.php
+++ portfolio.php
@@ -18,36 +18,46 @@ require 'code.php';
         <meta charset='UTF-8'>
     </head>
     <body>
-        <header class='pg vert'>
-            <section class='horiz'>
+        <header class='intro pg vert'>
+            <section class='horiz fullheight'>
                 <span class='title'><span class='name'></span><span class='cursor'>_</span></span>
             </section>
-            <section class='horiz'>
-                <article class='horiz'>
-                    <button class ='vert'>
+            <section class='horiz fullheight'>
+                <button class='who fade button1 horiz fullheight'>
+                    <div class='vert'>
                         <span>
-                            Who is this guy?
+                            who is this guy?
                         </span>
                         <span>
                             <i class='fas fa-user-secret largeIcon'></i>
                         </span>
-                    </button>
-                </article>
-                <article class='horiz'>
-                    <button class='vert'>
-                        <span>
-                            What can he do?
-                        </span>
-                        <span>
-                            <i class='fas fa-code largeIcon'></i>
-                        </span>
-                    </button>
-                </article>
+                    </div>
+                </button>
+                <button class='what fade button2 horiz fullheight'>
+                    <div class='vert'>
+                            <span>
+                                what can he do?
+                            </span>
+                            <span>
+                                <i class='fas fa-code largeIcon'></i>
+                            </span>
+                    </div>
+                </button>
             </section>
         </header>
         <main>
-            <div class='pg vert about'>
-                <section class='horiz'>
+            <div class='about pg vert'>
+                <div class='menu'>
+                    <button class='back button1 color1'>&#8592; back</button>
+                    <button class='what button2 color2'>what can he do? &#8594;</button>
+                </div>
+                <section class='horiz fullheight'>
+                    <article class='horiz fullheight'>
+                        <div class='image fullheight'>
+                        </div>
+                    </article>
+                </section>
+                <section class='horiz fullheight'>
                     <article class='vert'>
                         <span class='subtitle color1'>
                             Work
@@ -58,9 +68,6 @@ require 'code.php';
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
-                    </article>
-                    <article class='vert'>
-                        <img class='image' src='media/Profile photo (B&W).png'>
                     </article>
                     <article class='vert'>
                         <span class='subtitle color1'>
@@ -75,157 +82,10 @@ require 'code.php';
                     </article>
                 </section>
             </div>
-            <div class='pg vert portfolio'>
+            <div class='portfolio pg vert'>
 
             </div>
         </main>
 <!--        <img class='logo' src='--><?php //echo $php; ?><!--' alt='--><?php //echo $phpAlt; ?><!--'>-->
     </body>
 </html>
-
-
-<!--        -->
-<!--		<div id='menubar'>-->
-<!--			<a href='#projects'>-->
-<!--				Projects-->
-<!--			</a>-->
-<!--			<a href='#about'>-->
-<!--				About-->
-<!--			</a>-->
-<!--			<a href ='#contact'>-->
-<!--				Contact-->
-<!--			</a>-->
-<!--		</div>-->
-<!--        <div class='contact'>-->
-<!--            <a href='tel:07879646110'>-->
-<!--                <img src='media/phone.png' alt='Phone icon'>-->
-<!--            </a>-->
-<!--            <a href='mailto:richardcatterill@outlook.com'>-->
-<!--                <i class="fas fa-paper-plane"></i>-->
-<!--            </a>-->
-<!--            <a class='link' target='_blank' href='https://www.linkedin.com/in/richardcatterill'>-->
-<!--                <img src='media/linkedin.png' alt='LinkedIn logo'>-->
-<!--            </a>-->
-<!--        </div>-->
-<!--		<section id='projects'>-->
-<!--			<div class='sectiontitle'>-->
-<!--				<h1>Projects</h1>-->
-<!--			</div>-->
-<!--            <div class='projectContainer'>-->
-<!---->
-<!--            </div>-->
-<!--            <div class='github'>-->
-<!--                <a class='link' target='_blank' href='https://github.com/cattre'>-->
-<!--                    <div class='text'>-->
-<!--                        <div class='title'>-->
-<!--                            GitHub-->
-<!--                        </div>-->
-<!--                        <div class='value'>-->
-<!--                            github.com/cattre-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                    <div class='image'>-->
-<!--                        <img src='media/github.png' alt='GitHub logo'>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--		</section>-->
-<!--		<section id='about'>-->
-<!--			<div class='sectiontitle'>-->
-<!--				<h1>A little bit about me.</h1>-->
-<!--			</div>-->
-<!--			<div class='aboutimages'>-->
-<!--				<div>-->
-<!--					<img src='media/slide1.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide2.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide3.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide4.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide5.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide6.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide7.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide8.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--				<div>-->
-<!--					<img src='media/slide9.jpg' alt='Personal image'>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--			<p>-->
-<!--				Having looked over many a developer's shoulder during a ten-year career managing sofware projects, I finally decided to get stuck in myself and have embarked on a full stack software development course.-->
-<!--			</p>-->
-<!--			<p>-->
-<!--				Originally from London, we moved to Bath in 2016 and have been enjoying the delights of the South West ever since. Our next move is now calling and we will be returning to my wife's birthplace on the Cornwall coast.-->
-<!--			</p>-->
-<!--			<p>-->
-<!--				I try to get out for a run every now and then, although prefer a bit of kayaking and am therefore looking forward to making the most of being near the sea. I may even embarass myself on a surfboard. Our dog loves beaches/forests/fields and frequently takes us on adventures, which happily often end up at a pub.-->
-<!--			</p>-->
-<!--			<p>-->
-<!--				I am still learning the ropes at the moment but should be available for my first real challenge in early 2021.-->
-<!--			</p>-->
-<!--		</section>-->
-<!--		<section id='contact'>-->
-<!--			<div class='sectiontitle'>-->
-<!--				<h1>Get in touch.</h1>-->
-<!--			</div>-->
-<!--			<div class='contactbuttons'>-->
-<!--				<div class='phone'>-->
-<!--					<a href='tel:07879646110'>-->
-<!--						<div class='text'>-->
-<!--							<div class='title'>-->
-<!--								Phone-->
-<!--							</div>-->
-<!--							<div class='value'>-->
-<!--								07879 646 110-->
-<!--							</div>-->
-<!--						</div>-->
-<!--						<div class='image'>-->
-<!--							<img src='media/phone.png' alt='Phone icon'>-->
-<!--						</div>-->
-<!--					</a>-->
-<!--				</div>-->
-<!--				<div class='email'>-->
-<!--					<a href='mailto:richardcatterill@outlook.com'>-->
-<!--						<div class='text'>-->
-<!--							<div class='title'>				-->
-<!--								Email-->
-<!--							</div>-->
-<!--							<div class='value'>-->
-<!--								richardcatterill@outlook.com-->
-<!--							</div>-->
-<!--						</div>-->
-<!--						<div class='image'>-->
-<!--							<img src='media/sendmail.png' alt='Email icon'>-->
-<!--						</div>-->
-<!--					</a>-->
-<!--				</div>-->
-<!---->
-<!--				<div class='linkedin'>-->
-<!--					<a class='link' target='_blank' href='https://www.linkedin.com/in/richardcatterill'>-->
-<!--						<div class='text'>-->
-<!--							<div class='title'>-->
-<!--								LinkedIn-->
-<!--							</div>-->
-<!--							<div class='value'>-->
-<!--								www.linkedin.com/in/richardcatterill-->
-<!--							</div>-->
-<!--						</div>-->
-<!--						<div class='image'>-->
-<!--							<img src='media/linkedin.png' alt='LinkedIn logo'>-->
-<!--						</div>-->
-<!--					</a>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</section>-->
