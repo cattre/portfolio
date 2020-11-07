df077d0e8bedc9367eecab2911ffa94d6bc52030
Richard Catterill <r_catterill@hotmail.com>
2020-11-07 22:52:33 +0000

    updating splash

--- app/js/splash.js
+++ app/js/splash.js
@@ -2,11 +2,9 @@ let nameSpan = document.querySelector('.name')
 let name = ['R', 'i', 'c', 'h', 'a', 'r', 'd', ' ', 'C', 'a', 't', 't', 'e', 'r', 'i', 'l', 'l']
 let index = 0
 
-window.addEventListener('pageshow', () => {
-    setTimeout(() => {
-        typeName()
-    }, 500)
-})
+setTimeout(() => {
+    typeName()
+}, 500)
 
 typeName = () => {
     let string = ''
